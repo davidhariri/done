264973ab90fd6186c466758f1b3802f839894c85
David Hariri <david.hariri@gmail.com>
2017-03-08 09:06:22 -0500

    Making things simpler with just one model

--- Done/Todo.swift
+++ Done/Todo.swift
@@ -8,10 +8,29 @@
 
 import Foundation
 
-struct Todo {
-    let text: String
-    let done: Bool
+class Todo {
+    var text: String
+    var done: Bool
     let created: Date
-    let updated: Date
+    var updated: Date?
+    
+    init() {
+        self.text = ""
+        self.done = false
+        self.created = Date()
+        self.updated = nil
+    }
+    
+    func toggleDone() {
+        self.done = !self.done
+    }
+    
+    func wasUpdated() {
+        self.updated = Date()
+    }
+    
+    func updateText(newText: String) {
+        self.text = newText
+        self.wasUpdated()
+    }
 }
-
