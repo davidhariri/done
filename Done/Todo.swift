2c9b6d9212fa9a6f5f5bca56f9f7f9abcbfc28ed
David Hariri <david.hariri@gmail.com>
2017-03-08 08:48:19 -0500

    Adds basic Todo model

---
+++ Done/Todo.swift
@@ -0,0 +1,17 @@
+//
+//  Todo.swift
+//  Done
+//
+//  Created by David Hariri on 2017-03-08.
+//  Copyright © 2017 David Hariri. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    let text: String
+    let done: Bool
+    let created: Date
+    let updated: Date
+}
+
