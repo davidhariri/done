11def5a40506479d529ae8a685204483fe52c16c
David Hariri <david.hariri@gmail.com>
2017-03-09 08:22:58 -0500

    Learned about XCTest and implemented first unit tests

---
+++ DoneTests/DoneTests.swift
@@ -0,0 +1,36 @@
+//
+//  DoneTests.swift
+//  DoneTests
+//
+//  Created by David Hariri on 2017-03-09.
+//  Copyright © 2017 David Hariri. All rights reserved.
+//
+
+import XCTest
+@testable import Done
+
+class TodoNewTests: XCTestCase {
+    var todo: Todo!
+    
+    override func setUp() {
+        super.setUp()
+        todo = Todo(text: "Todo")
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        todo = nil
+    }
+    
+    func testThatIsIsNotDone() {
+        XCTAssert(todo.done == false, ".done was not false")
+    }
+    
+    func testThatItHasText() {
+        XCTAssert(todo.text == "Test", ".text was not 'Test'. Got '\(todo.text)'")
+    }
+    
+    func testThatItHasNoUpdatedValue() {
+        XCTAssert(todo.updated == nil)
+    }
+}
